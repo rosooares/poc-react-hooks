8bbb41d24adb8774454ad2aa17c1286064a6fe0a
Kestraa <kestraa@MacBook-Pro-de-FVFYV6XLL40Y.local>
2019-11-19 10:30:27 -0300

    favorite states

--- githooks/src/App.js
+++ githooks/src/App.js
@@ -1,27 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    {id: 1, name: 'repo-1' },
-    {id: 2, name: 'repo-2' },
-    {id: 3, name: 'repo-3' },
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepository(){
-    setRepositories([...repositories, {id: Math.random(), name: 'Novo repo'}])
+  useEffect(async () => {
+    const response = await fetch('http://servicodados.ibge.gov.br/api/v1/localidades/estados');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Voce tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo;
+    });
+    setRepositories(newRepositories);
   }
 
-  return (
+  return (  
     <>
+    <h1> Estados, selecione o Favorito! </h1>
     <ul>
-      {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+      {repositories.map(repo => (  
+        <li key={repo.id}>
+          {repo.nome}
+          {repo.favorite && <span> * Favorito * </span>}
+          <button onClick={() => handleFavorite(repo.id)}>
+            Favoritar 
+          </button>
+        </li>
       ))}
     </ul>
-    <button onClick={handleAddRepository}>
-      Adicionar repositório
-    </button>
-    </>
+  </>
   );
 }
 
